4f2a537bf5845f38f2fbfa7985d04ab02e67ca14
nson95 <nicksonnenberg95@gmail.com>
2025-04-30 21:24:13 -0400

    fixed logic in console library promptreqstring method

---
+++ 05-01DiceRollerProj/src/DiceRollerApp.java
@@ -0,0 +1,10 @@
+import util.MyConsole;
+
+public class DiceRollerApp {
+
+	public static void main(String[] args) {
+		MyConsole.printLine("Dice Roller\n\n");
+		MyConsole.print("Goodbye");
+	}
+
+}
--- proj07-04-dice-roller-app/src/DiceRollerApp.java
+++ proj07-04-dice-roller-app/src/DiceRollerApp.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+
 import util.MyConsole;
 
 public class DiceRollerApp {
 	public static void main(String []argh)
 	{
-		MyConsole.printLine();
+		MyConsole.printLine("Welcome to the dice roller app");
+		
+		int i = 0;
+		Random randy = new Random();
+		
+		
+		
+		MyConsole.printLine("Goodbye");
 	}
 	
 }
