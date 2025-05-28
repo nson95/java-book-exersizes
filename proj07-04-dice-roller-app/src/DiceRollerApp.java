8c1a5fb1d2fd0c3153a3460dcd0a185042a9e5bc
nson95 <nicksonnenberg95@gmail.com>
2025-05-28 13:39:03 -0400

    finished dicerollerapp

--- proj07-04-dice-roller-app/src/DiceRollerApp.java
+++ proj07-04-dice-roller-app/src/DiceRollerApp.java
@@ -10,7 +10,20 @@ public class DiceRollerApp {
 		int i = 0;
 		Random randy = new Random();
 		String choice = "y";
-		randy.nextInt(1,7);
+		while (choice.equals("y")) {
+			int die1 = randy.nextInt(1,7);
+			int die2 = randy.nextInt(1,7);
+			MyConsole.printLine("Die 1: "+die1);
+			MyConsole.printLine("Die 2: "+die2);
+			MyConsole.printLine("Total: "+(die1+die2));
+			if (die1+die2==12) {
+				MyConsole.printLine("Box Cars!");
+			}
+			else if (die1+die2==2) {
+				MyConsole.printLine("Snake Eyes!");
+			}
+			choice = MyConsole.promptReqString("Continue?: (y/n)", "y", "n");
+		}
 		MyConsole.printLine("Goodbye");
 	}
 	
