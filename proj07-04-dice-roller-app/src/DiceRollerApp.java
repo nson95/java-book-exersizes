91bbf91bfb97398b890552d9cbf60f6be6f8802a
nson95 <nicksonnenberg95@gmail.com>
2025-05-29 23:09:18 -0400

    catchup commit

--- proj07-04-dice-roller-app/src/DiceRollerApp.java
+++ proj07-04-dice-roller-app/src/DiceRollerApp.java
@@ -6,7 +6,6 @@ public class DiceRollerApp {
 	public static void main(String []argh)
 	{
 		MyConsole.printLine("Welcome to the dice roller app");
-		
 		int i = 0;
 		Random randy = new Random();
 		String choice = "y";
